308ec13e5b04f6e8ba526bb3f056a8811507a65a
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2022-01-27 10:55:52 +0100

    add vue in home.guest

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+       
+       <h1>Qui contentuto App.vue</h1>
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'App',
+        
+       
+    }
+</script>
--- resources/js/vue.js
+++ resources/js/vue.js
@@ -3,6 +3,7 @@
 window.Vue = require('vue');
 
 import App from './components/App.vue';
+
 const app = new Vue({
     el: '#root',
     render: h => h(App)
