024e7d59819c7c49a25f7efd8db2657bd59e6669
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-27 17:12:22 +0100

    changes movies route

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/api", "Api/MoviesController@index");
+Route::get("/movies", "Api\MoviesController@index");
