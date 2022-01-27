c747ab0f4d7c7b1f908c4429171070d288f65d7d
GiulianoPrimavera <giuliano.plrimavera@gmail.com>
2022-01-27 17:05:53 +0100

    adds function index that returns all the data

--- app/Http/Controllers/Api/MoviesController.php
+++ app/Http/Controllers/Api/MoviesController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Movie;
 use Illuminate\Http\Request;
 
 class MoviesController extends Controller
 {
-    //
+    public function index(){
+        $moviesData = Movie::all();
+
+        return response()->json($moviesData);
+    }
 }
